cf3dd46297f10ba301a5c2c1a1d49289e2d05b69
Moaaiedm <moayed10111@gmail.com>
2021-11-27 14:59:50 -0500

    added login , otp and reset

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,23 @@
 const authRoutes = require('./auth.route');
+
 const defaultRoutes = [
     {
         path: "/auth",
         route: authRoutes
+    },
+    {
+        path:"/login",
+        route: authRoutes
+    },
+    {
+        path:"/otp",
+        route:authRoutes
+    },
+    {
+        path:'/resetPassword',
+        route:authRoutes
     }
+
 ]
 
 module.exports = defaultRoutes;
